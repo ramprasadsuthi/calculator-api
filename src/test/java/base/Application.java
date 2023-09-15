06428567968ce7428449fc9b1034736232b6fbe0
ramprasadsuthi <ramprasadsuthi@gmail.com>
2023-09-15 10:48:55 +0530

    updated by Raj
    some function in Application.java

--- src/test/java/base/Application.java
+++ src/test/java/base/Application.java
@@ -8,5 +8,9 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        //Below code is added by Raj
+        // line-1
+        // line-2
+        // line-3
     }
 }
